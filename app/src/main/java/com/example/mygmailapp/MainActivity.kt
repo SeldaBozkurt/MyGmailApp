fcc7118b9d8bfc7861bc33a0061aef5ec5640ed6
SeldaBozkurt <88602751+SeldaBozkurt@users.noreply.github.com>
2022-01-21 14:32:46 +1100

    adding all files

---
+++ app/src/main/java/com/example/mygmailapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.mygmailapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mygmailapp.components.*
+import com.example.mygmailapp.ui.theme.MyGmailAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyGmailAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    GmailApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GmailApp() {
+
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
+    val openDialog = remember {
+        mutableStateOf(false)
+    }
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { HomeAppBar(scaffoldState, coroutineScope, openDialog) },
+        drawerContent = {
+            GmailDrawerMenu(scrollState)
+
+        },
+        bottomBar = {
+            HomeBottomMenu()
+        },
+        floatingActionButton = { GmailFab(scrollState) }
+    ) {
+
+        MailList(paddingValues = it, scrollState)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyGmailAppTheme {
+        GmailApp()
+    }
+}
---
+++ app/src/main/java/com/example/mygmailapp/model/Account.kt
@@ -0,0 +1,8 @@
+package com.example.mygmailapp.model
+
+class Account (
+    val icon: Int? = null,
+    val userName: String,
+    val email: String,
+    val unReadMails: Int
+)
